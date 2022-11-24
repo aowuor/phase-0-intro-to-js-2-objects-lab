81a8a0d9c8bb3e3f8a733c693c2f64adfbe050be
aowuor <angelaowuor@gmail.com>
2022-11-24 23:56:27 +0300

    commit tree b5f0e3f8250ecbb1e103e363b7a167e59ac3d842

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+const employee = {
+    name: "",
+    streetAddress: "",
+};
+
+function beforeEach() {
+    for (const key in employee) {
+      delete employee[key];
+    }
+    employee.name = 'Sam';
+};
+
+//Q1
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    beforeEach();
+    const newEmployee = {...obj};
+   newEmployee[key]= value;
+   return newEmployee;
+}
+
+
+//Q2
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    beforeEach();
+    obj[key] = value
+    return obj;
+}
+
+
+//Q3
+function deleteFromEmployeeByKey(obj, key){
+    beforeEach();
+    const newObj = {...obj};
+    delete newObj[key]
+    return newObj;
+}
+//let newEmployee = deleteFromEmployeeByKey(employee, 'name')
+
+
+//Q4
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    beforeEach();
+    delete obj[key]
+    return obj;
+}
+let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
